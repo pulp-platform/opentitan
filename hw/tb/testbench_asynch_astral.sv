2258637a555230d0b6bf38a0a74ef1ad3479778b
yvantor <yvan.tortorella@gmail.com>
2026-03-02 17:34:46 +0100

    Use proper core ID parameter

--- hw/tb/testbench_asynch_astral.sv
+++ hw/tb/testbench_asynch_astral.sv
@@ -310,7 +310,7 @@ module testbench_asynch_astral ();
 /////////////////////////////// DUT ///////////////////////////////
 
 
-   security_island #(.HartIdOffs(0)) dut (
+   security_island #(.HartIdOffs(4)) dut (
        .clk_i            ( clk_sys       ),
        .clk_ref_i        ( clk_sys       ),
        .rst_ni           ( rst_sys_n     ),
